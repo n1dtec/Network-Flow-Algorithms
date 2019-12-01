f7ec3dd3c613c26d25178a3764924d93ee1deabd
zjr19950924 <zjr19950924@zjr19950924deMacBook-Air.local>
2019-11-30 18:55:58 -0800

    change scalingFordFulkerson

--- preflowpush/src/algorithms/ScalingFordFulkerson.java
+++ preflowpush/src/algorithms/ScalingFordFulkerson.java
@@ -13,32 +13,39 @@ public class ScalingFordFulkerson {
     private static String source;
     private static String sink;
 
-    public static int calculateMaxFlow(SimpleGraph sg, String source, String sink) {
+    public static String calculateMaxFlow(SimpleGraph sg, String source, String sink) {
         ScalingFordFulkerson.source = source;
         ScalingFordFulkerson.sink = sink;
-        return calculateMaxFlow(sg);
+        return ScalingFordFulkerson(sg);
     }
 
-    public static int calculateMaxFlow(SimpleGraph sg) {
+    public static String ScalingFordFulkerson(SimpleGraph sg) {
 
         int maxflow = 0;
+        long start, stop;
+        // Current Time at the start of finding maxflow
+        start = System.currentTimeMillis();
         int flow = 0;
         int cnt = 0;
         int delta = Integer.MAX_VALUE;
+        //repeatedly find augmenting paths from source to sink using only edges
         while (delta > 0) {
 
-            while ((flow = AugmentPath(sg, delta)) > 0) {
+            while ((flow = OtherPath(sg, delta)) > 0) {
                 maxflow += flow;
             }
             delta /= 2;
         }
-        return maxflow;
+        // Current Time at the end of finding maxFlow
+        stop = System.currentTimeMillis();
+        // Return the result
+        return "Runtime: " + (stop - start) + " ms" + "	Max Flow: " + maxflow;
     }
 
-    public static int AugmentPath(SimpleGraph sg, int delta) {
+    public static int OtherPath(SimpleGraph graph, int d) {
         int flow = 0;
         Vertex s = null;
-        for (Iterator ite = sg.vertices(); ite.hasNext();) {
+        for (Iterator ite = graph.vertices(); ite.hasNext();) {
             Vertex v = (Vertex) ite.next();
             if (v.getName().equals(source)) {
                 s = v;
@@ -62,7 +69,7 @@ public class ScalingFordFulkerson {
                 Edge e = (Edge) ite.next();
                 Vertex u = e.getSecondEndpoint();
                 Vertex next = (Vertex) visited.get(u.getName());
-                if (next == null && (Integer) e.getData() >= delta) {
+                if (next == null && (Integer) e.getData() >= d) {
                     list.add(u);
                     flist.add(Integer.min((Integer) flist.get(i), (Integer) e.getData()));
                     edges.add(e);
@@ -80,21 +87,19 @@ public class ScalingFordFulkerson {
             }
 
         }
-        updateResGraph(sg, list, last, edges, flow);
-        return flow;
-    }
-
-    private static void updateResGraph(SimpleGraph sg, ArrayList list, ArrayList last, ArrayList edges, int flow) {
-
+        //update the response graph
         for (int i = list.size() - 1; i != 0;) {
             int j = (Integer) last.get(i);
             Vertex u = (Vertex) list.get(i);
             Vertex v = (Vertex) list.get(j);
             Edge e = (Edge) edges.get(i);
-            sg.insertEdge(u, v, flow, null);
+            graph.insertEdge(u, v, flow, null);
             e.setData((Integer) e.getData() - flow);
             i = j;
         }
+
+        return flow;
     }
+
 }
 
